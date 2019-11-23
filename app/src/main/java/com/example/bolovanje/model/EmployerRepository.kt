746332f9ff257126062b460f41c1107c55da5217
Matina Matic <mat.matic@levi9.com>
2019-11-23 18:41:18 +0100

    Finished calendar and all employers

--- app/src/main/java/com/example/bolovanje/model/EmployerRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.bolovanje.model
-
-import javax.inject.Inject
-import javax.inject.Singleton
-
-@Singleton
-class EmployerRepository @Inject constructor(val employer: Employer) {
-
-}
